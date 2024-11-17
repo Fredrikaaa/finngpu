c3fe5b0639ef423d09ecf13241dbff42c03ab560
fredrikaaa <fredrik00@pm.me>
2024-11-17 13:57:07 +0100

    Updated skip terms

--- config.py
+++ config.py
@@ -30,7 +30,7 @@ BLACKLIST_FILE = BASE_DIR / "blacklist.txt"
 WHITELIST_FILE = BASE_DIR / "whitelist.txt"
 
 # Listing processing
-SKIP_TERMS = ['pc', 'stasjonær', 'mining', 'mac', 'laptop', 'dell', 'hp', 'gamingpoint', 'datamaskin', 'server', 'windows', 'lenovo', 'desktop']
+SKIP_TERMS = ['pc', 'stasjonær', 'mining', 'mac', 'laptop', 'dell', 'gamingpoint', 'datamaskin', 'server', 'windows', 'lenovo', 'desktop', 'kjøler']
 
 # Output settings
 OUTPUT_FORMAT = "finn_gpu_listings_{date}.csv"
